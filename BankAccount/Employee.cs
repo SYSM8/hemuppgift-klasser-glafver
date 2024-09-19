2494a0271247c901c9eff0ac8dc4871117d7a39a
glafver <glafver22@gmail.com>
2024-09-19 15:11:26 +0200

    Implemented Employee and Bank Account

---
+++ BankAccount/Employee.cs
@@ -0,0 +1,33 @@
+﻿using BankAccount;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BankAccount
+{
+    internal class Employee
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Salary { get; set; }
+
+        public Employee(string name, string surname)
+        {
+            Name = name;
+            Surname = surname;
+        }
+        public int GetAnnualSalary()
+        {
+            return Salary * 12;
+        }
+
+        public void GiveRaise(double percentage)
+        {
+            double increase = Salary * (percentage / 100);
+            Salary = (int)(Salary + increase);
+        }
+    }
+}
